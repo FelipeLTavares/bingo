f0e7316223d62eb64b35ec9c9babebd75abf10e6
Felipe Tavares <felipelimatavares96@gmail.com>
2024-09-22 17:51:31 -0300

    New ajusts

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-app theme="light">
-      <router-view />
-  </v-app>
-</template>
-
-<script lang="ts" setup>
-  //
-</script>
--- src/utils/selectRandomNumber.ts
+++
@@ -1,9 +0,0 @@
-export const selectRandomNumber = (selectedNumbers: number[]): any=> {
-    const randomNumber = Math.floor(Math.random() * 76);
-    if(selectedNumbers.length > 74) return;
-    if (selectedNumbers.includes(randomNumber)) {
-        return selectRandomNumber(selectedNumbers);
-    } else {
-        return randomNumber;
-    }
-};
